ac9b2fe44f20df77791c1caa1df17350a36db140
baldy777 <jhoelquispetapia315@gmail.com>
2025-11-12 19:49:43 -0400

    feat: adicion de entidad usuarioRol con ORM

---
+++ src/modules/usuarios/entities/usuarioRol.ts
@@ -0,0 +1,35 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+} from "typeorm";
+import { Usuario } from "./usuario";
+import { Rol } from "./rol";
+
+@Entity("usuario_rol")
+export class UsuarioRol {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: "int" })
+  usuario_id!: number;
+
+  @Column({ type: "int" })
+  rol_id!: number;
+
+  @CreateDateColumn()
+  created_at!: Date;
+
+  @ManyToOne(() => Usuario, (usuario) => usuario.usuarioRoles, {
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "usuario_id" })
+  usuario!: Usuario;
+
+  @ManyToOne(() => Rol, (rol) => rol.usuarioRoles, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "rol_id" })
+  rol!: Rol;
+}
